0084e6feceea878f450dd92a49ce485e43367814
jverbosky <jverbosk@gmail.com>
2017-04-26 15:23:56 -0400

    added push notification overview diagram

--- app.rb
+++ app.rb
@@ -1,32 +1,20 @@
 require 'sinatra'
 require_relative 'ios_post_ops.rb'
 
-# class FirebaseServerPrototypeApp < Sinatra::Base
+get "/" do
+  erb :start
+end
 
-  get "/" do
-    erb :start
-  end
-
-  # Route to output post data from iOS app to text file (use for testing/troubleshooting)
-  # post '/post_id' do
-  #   id_hash = {"email"=>params[:email], "fcm_id"=>params[:fcm_id]}
-  #   write_file(id_hash)  # save user_hash to a file
-  #   "Post successful - thanks for the info!"
-  # end
-
-  # Route to insert post data from iOS app into SQLite database (pre-production)
-  post '/post_id' do
-    id_hash = {"email"=>params[:email], "fcm_id"=>params[:fcm_id]}
-    write_db(id_hash)  # save user_hash to a file
-    "Post successful - thanks for the info!"
-  end
-
-  # post '/post_id' do
-  #   id = params[:id]
-  #   "#{id}"
-  #   # write_db(id_hash)
-  #   # instance_id = get_data(id_hash[1])
-  #   # erb :get_id, locals: {id_hash: id_hash}
-  # end
-
+# Route to output post data from iOS app to text file (use for testing/troubleshooting)
+# post '/post_id' do
+#   id_hash = {"email"=>params[:email], "fcm_id"=>params[:fcm_id]}
+#   write_file(id_hash)  # save user_hash to a file
+#   "Post successful - thanks for the info!"
 # end
+
+# Route to insert post data from iOS app into SQLite database (pre-production)
+post '/post_id' do
+  id_hash = {"email"=>params[:email], "fcm_id"=>params[:fcm_id]}
+  write_db(id_hash)  # save user_hash to a file
+  "Post successful - thanks for the info!"
+end
--- ios_post_ops.rb
+++ ios_post_ops.rb
@@ -1,6 +1,6 @@
 require 'sqlite3'
 
-# Method to output data from iOS app post to a text file
+# Method to output data from iOS app post to a text file (use for testing/troubleshooting)
 def write_file(post_data)
   File.open("public/post_data.txt","w") do |f|  # open the user.json file in the /public directory (create if not present)
     f.write(post_data)  # add the hash to the JSON file and save it
@@ -16,25 +16,42 @@ def read_db()
   end
 end
 
-# Method to add current instance_id hash to SQLite db
+# Method to add current fcm_id hash to SQLite db
 def write_db(id_hash)
   db = read_db() # open database for updating
   db.results_as_hash  # determine current max index (id) in details table
   max_id = db.execute('select max("id") from tokens')[0][0]
   max_id == nil ? id = 1 : id = max_id + 1  # set index variable based on current max index value
-  email = id_hash["email"]  # prepare data from user_hash for database insert
+  email = id_hash["email"]  # prepare data from id_hash for database insert
   fcm_id = id_hash["fcm_id"]
   db.execute('insert into tokens (id, email, fcm_id)
               values(?, ?, ?)', [id, email, fcm_id])
 end
 
 # Method to update existing record for device
-# Write after detetermining what unique value to pass from iOS app
 def update_db(id_hash)
+  email = id_hash["email"]  # get email address for db record selection
+  db = read_db() # open database for updating
+  db.execute(#need to research SQLite update statement syntax)
+
+    id = user_hash["id"]  # determine the id for the current record
+    conn = open_db() # open database for updating
+    user_hash.each do |column, value|  # iterate through user_hash for each column/value pair
+      unless column == "id"  # we do NOT want to update the id
+        table = match_table(column)  # determine which table contains the specified column
+        value = get_image_name(user_hash) if column == "image"  # get image name from nested array
+        # workaround for table name being quoted and column name used as bind parameter
+        query = "update " + table + " set " + column + " = $2 where id = $1"
+        conn.prepare('q_statement', query)
+        rs = conn.exec_prepared('q_statement', [id, value])
+        conn.exec("deallocate q_statement")
+      end
+
+
 
 end
 
-# Method to return instance_id hash from SQLite db for specified instance_id
+# Method to return instance_id hash from SQLite db for specified fcm_id
 def get_data(fcm_id)
   db = read_db()
   db.results_as_hash = true
