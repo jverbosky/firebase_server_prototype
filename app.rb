6a76301672dfcc7b4ffa2dacbc5e16b9e53225a0
jverbosk <jverbosk@gmail.com>
2017-05-01 17:17:34 -0400

    updated /post route to include pne_status

--- app.rb
+++ app.rb
@@ -24,7 +24,7 @@ end
 
 # Route to insert post data from iOS app into SQLite database (pre-production)
 post '/post_id' do
-  id_hash = {"email"=>params[:email], "fcm_id"=>params[:fcm_id]}
+  id_hash = {"email"=>params[:email], "pne_status"=>params[:pne_status], "fcm_id"=>params[:fcm_id]}
   check_db(id_hash)  # update/insert record with fcm_id
   "Post successful - thanks for the info!"  # feedback for Xcode console (successful POST)
 end
